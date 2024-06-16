695c36f63767625a7e8bcb02aa929cd91861bf29
sahinkayar <146575142+sahinkayar@users.noreply.github.com>
2024-06-16 22:00:17 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { GlobalProvider } from "./context/GlobalContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <GlobalProvider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </GlobalProvider>
 );
